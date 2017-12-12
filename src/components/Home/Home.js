271e5873a5d5ba35bef9e071b0fb22d455726698
Clayton Pabst <claytonpabst@gmail.com>
2017-12-12 03:46:10 -0700

    This is hard....

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,12 +4,50 @@ import './Home.css';
 
 
 class Home extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {
+
+    }
+    //bind shit here
+  }
 
   render() {
+
+    let testStyle = [
+      {
+        "height":"100px",
+        "width":"50px",
+        "background":"black",
+        "left":"200px",
+
+      },
+    ]
+
+    let moveTheDiv = function(e){
+      return {
+        startMoving: function(e){
+          console.log(e)
+          let mouseDownX = e.clientX;
+          let mouseDownY = e.clientY;
+          let mouseUpX;
+          let mouseUpY;
+
+          document.onmousemove = function(e){
+            console.log(e)
+          }
+
+        }
+
+      }
+
+    }
+
     return (
       <div className="home">
 
-          Home Page
+        <div draggable="true" onMouseDown={moveTheDiv.startMoving} style={testStyle[0]} className="createdDiv"></div>
 
       </div>
     );
