f8603f0b8c46637d4b2012bff376a498bbb9ae00
loren pabst <lorenpabst@gmail.com>
2017-12-12 23:58:25 -0700

    added code for a stopwatch component

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Home from './components/Home/Home.js';
+import Stopwatch from './components/Stopwatch/Stopwatch.js';
 
 
 export default (
     <Switch>
         
         <Route component={ Home } path='/' exact />
+        <Route component={ Stopwatch } path='/test' exact />
 
     </Switch>
 )
